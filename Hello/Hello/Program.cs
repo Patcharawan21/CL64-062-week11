aab205ca5ad2883638146707baf3e8556683875d
Patcharawan21 <63030062@KMITL.AC.TH>
2021-12-12 15:25:20 +0700

    add new command

--- Hello/Hello/Program.cs
+++ Hello/Hello/Program.cs
@@ -11,6 +11,7 @@ namespace Hello
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Computer Lab.");
+            Console.WriteLine("This is new command add for exercise.");
             Console.ReadKey();
         }
     }
